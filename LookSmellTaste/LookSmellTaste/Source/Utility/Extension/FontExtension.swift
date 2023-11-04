cbf901d6fd620a6637331cf3bb1a24d76500983d
hyunjun <hyunjuntyler@gmail.com>
2023-11-05 03:35:28 +0900

    :lipstick: 폰트 시스템 적용 (GmarketSans, TossFace)

---
+++ LookSmellTaste/LookSmellTaste/Source/Utility/Extension/FontExtension.swift
@@ -0,0 +1,24 @@
+//
+//  FontExtension.swift
+//  LookSmellTaste
+//
+//  Created by hyunjun on 11/5/23.
+//
+
+import SwiftUI
+
+extension Font {
+    static let tossFace = Font.custom("TossFaceFontMac", size: 17, relativeTo: .body)
+
+    static let gmarketSansLargeTitle = Font.custom("GmarketSansTTFMedium", size: 34, relativeTo: .largeTitle)
+    static let gmarketSansTitle = Font.custom("GmarketSansTTFMedium", size: 28, relativeTo: .title)
+    static let gmarketSansTitle2 = Font.custom("GmarketSansTTFMedium", size: 22, relativeTo: .title2)
+    static let gmarketSansTitle3 = Font.custom("GmarketSansTTFMedium", size: 20, relativeTo: .title3)
+    static let gmarketSansHeadline = Font.custom("GmarketSansTTFMedium", size: 17, relativeTo: .headline)
+    static let gmarketSansBody = Font.custom("GmarketSansTTFMedium", size: 17, relativeTo: .body)
+    static let gmarketSansCallout = Font.custom("GmarketSansTTFMedium", size: 16, relativeTo: .callout)
+    static let gmarketSansSubHeadline = Font.custom("GmarketSansTTFMedium", size: 15, relativeTo: .subheadline)
+    static let gmarketSansFootnote = Font.custom("GmarketSansTTFMedium", size: 13, relativeTo: .footnote)
+    static let gmarketCaption = Font.custom("GmarketSansTTFMedium", size: 12, relativeTo: .caption)
+    static let gmarketCaption2 = Font.custom("GmarketSansTTFMedium", size: 11, relativeTo: .caption2)
+}
