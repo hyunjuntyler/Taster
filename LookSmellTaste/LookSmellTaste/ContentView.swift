4f373b47d517fcc971af2673f6c6dae133a14b02
hyunjun <hyunjuntyler@gmail.com>
2023-11-05 01:53:05 +0900

    :tada: 프로젝트 시작

---
+++ LookSmellTaste/LookSmellTaste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LookSmellTaste
+//
+//  Created by hyunjun on 11/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LookSmellTaste/LookSmellTaste/LookSmellTasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookSmellTasteApp.swift
+//  LookSmellTaste
+//
+//  Created by hyunjun on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LookSmellTasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
