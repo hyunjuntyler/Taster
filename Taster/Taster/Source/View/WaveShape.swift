98b358e64b2b67cf9149023d442a398d472e3337
hyunjun <hyunjuntyler@gmail.com>
2024-10-07 08:15:43 +0900

    🍱 위스키 카테고리 이미지 추가

---
+++ Taster/Taster/Source/View/WaveShape.swift
@@ -0,0 +1,8 @@
+//
+//  WaveShape.swift
+//  Taster
+//
+//  Created by hyunjun on 10/7/24.
+//
+
+import Foundation
---
+++ Taster/Taster/Source/View/WineView.swift
@@ -0,0 +1,18 @@
+//
+//  WineView.swift
+//  Taster
+//
+//  Created by hyunjun on 10/7/24.
+//
+
+import SwiftUI
+
+struct WineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WineView()
+}
